839e1c8555652166c87356717627daaa36a57f69
jonghwan159 <whdghks1999@naver.com>
2025-05-14 11:58:51 +0900

    RRT clear

--- 04_PathPlanning/ex07_RRT.py
+++ 04_PathPlanning/ex07_RRT.py
@@ -15,13 +15,12 @@ class Node(object):
 
 class RRT(object):
     def __init__(self, start, goal, space, obstacle_list, success_dist_thres=1.0):
-        self.start_node = Node(start[0], start[1])  # node (x, y)
-        self.goal_node = Node(goal[0], goal[1])  # node (x, y)
-        self.space = space  # (min_x, max_x, min_y, max_y)
-        self.obstalce_list = obstacle_list  # list of (x, y ,r)
+        self.start_node = Node(start[0], start[1])
+        self.goal_node = Node(goal[0], goal[1])
+        self.space = space
+        self.obstalce_list = obstacle_list
         self.node_list = []
 
-        # options
         self.max_iter = 5000
         self.goal_sample_rate = 0.1
         self.min_u = 1.0
@@ -33,83 +32,80 @@ class RRT(object):
     def plan(self):
         self.node_list = [self.start_node]
         for i in range(self.max_iter):
-            # Get random node
             rand_node = self.get_random_node()
-            # Find neareast node
             nearest_node = self.find_nearest_node(self.node_list, rand_node)
-            # Create new node
-            u = self.stepsize*self.get_random_input(self.min_u, self.max_u)
+            u = self.stepsize * self.get_random_input(self.min_u, self.max_u)
             new_node = self.create_child_node(nearest_node, rand_node, u)
-            # Collision check(Node, Path)
-            node_collide = self.is_collide(new_node, self.obstalce_list)
-            if node_collide:
+            if self.is_collide(new_node, self.obstalce_list):
                 continue
-            collide = self.is_path_collide(nearest_node, new_node, self.obstalce_list, self.collision_check_step)
-            if collide:
+            if self.is_path_collide(nearest_node, new_node, self.obstalce_list, self.collision_check_step):
                 continue
-            # Add to tree
             new_node.set_parent(nearest_node)
             self.node_list.append(new_node)
-            # Goal check
-            goal_reached = self.check_goal(
-                new_node, self.success_dist_thres)
-            if goal_reached:
+            if self.check_goal(new_node, self.success_dist_thres):
                 print(" [-] GOAL REACHED")
                 return self.backtrace_path(new_node)
         return None
 
     @staticmethod
     def is_same_node(node1, node2):
-        # Code
-        return False
+        return node1.x == node2.x and node1.y == node2.y
 
     def backtrace_path(self, node):
-        current_node = node
-        path = [current_node]
-        reached_start_node = self.is_same_node(current_node, self.start_node)
-        while not reached_start_node:
-            current_node = current_node.parent
-            path.append(current_node)
-            reached_start_node = self.is_same_node(
-                current_node, self.start_node)
+        path = [node]
+        while node.parent is not None:
+            node = node.parent
+            path.append(node)
         return path[::-1]
 
     def get_random_node(self):
-        # Code
-        # Recommendation : set random node as goal node occasionally
-        if 1:
-            return rand_node
-        else:
+        if np.random.rand() < self.goal_sample_rate:
             return self.goal_node
+        x = np.random.uniform(self.space[0], self.space[1])
+        y = np.random.uniform(self.space[2], self.space[3])
+        return Node(x, y)
 
     def check_goal(self, node, success_dist_thres):
-        # Code
-        return False
+        dx = node.x - self.goal_node.x
+        dy = node.y - self.goal_node.y
+        return np.hypot(dx, dy) <= success_dist_thres
 
     @staticmethod
     def create_child_node(nearest_node, rand_node, u):
-        # Code
-        new_node = Node(new_x, new_y)
-        return new_node
+        theta = np.arctan2(rand_node.y - nearest_node.y, rand_node.x - nearest_node.x)
+        new_x = nearest_node.x + u * np.cos(theta)
+        new_y = nearest_node.y + u * np.sin(theta)
+        return Node(new_x, new_y)
 
     @staticmethod
     def get_random_input(min_u, max_u):
-        # Code
         return np.random.uniform(min_u, max_u)
 
     @staticmethod
     def find_nearest_node(node_list, rand_node):
-        # Code
+        dists = [np.hypot(node.x - rand_node.x, node.y - rand_node.y) for node in node_list]
+        min_index = int(np.argmin(dists))
         return node_list[min_index]
 
     @staticmethod
     def is_collide(node, obstacle_list):
-        # Code
+        for ox, oy, r in obstacle_list:
+            if np.hypot(node.x - ox, node.y - oy) <= r:
+                return True
         return False
 
     @staticmethod
     def is_path_collide(node_from, node_to, obstacle_list, check_step=0.2):
-        # Code
+        dx = node_to.x - node_from.x
+        dy = node_to.y - node_from.y
+        dist = np.hypot(dx, dy)
+        steps = int(dist / check_step)
+        for i in range(steps):
+            x = node_from.x + dx * i / steps
+            y = node_from.y + dy * i / steps
+            for ox, oy, r in obstacle_list:
+                if np.hypot(x - ox, y - oy) <= r:
+                    return True
         return False
 
 
@@ -119,26 +115,27 @@ if __name__ == "__main__":
     success_dist_thres = 1.0
     rrt = RRT(start, goal, space, obstacle_list, success_dist_thres)
     path = rrt.plan()
-    for node in path:
-        print(" [-] x = %.2f, y = %.2f " % (node.x, node.y))
+    if path:
+        for node in path:
+            print(" [-] x = %.2f, y = %.2f " % (node.x, node.y))
 
-    # draw result
-    _t = np.linspace(0, 2*np.pi, 30)
-    for obs in obstacle_list:
-        x, y, r = obs
-        _x = x + r * np.cos(_t)
-        _y = y + r * np.sin(_t)
+        _t = np.linspace(0, 2 * np.pi, 30)
+        for obs in obstacle_list:
+            x, y, r = obs
+            _x = x + r * np.cos(_t)
+            _y = y + r * np.sin(_t)
+            plt.plot(_x, _y, 'k-')
 
-        plt.plot(_x, _y, 'k-')
+        goal_x = goal[0] + success_dist_thres * np.cos(_t)
+        goal_y = goal[1] + success_dist_thres * np.sin(_t)
+        plt.plot(goal_x, goal_y, 'g--')
 
-    goal_x = goal[0] + success_dist_thres * np.cos(_t)
-    goal_y = goal[1] + success_dist_thres * np.sin(_t)
-    plt.plot(goal_x, goal_y, 'g--')
+        for i in range(len(path) - 1):
+            node_i = path[i]
+            node_ip1 = path[i + 1]
+            plt.plot([node_i.x, node_ip1.x], [node_i.y, node_ip1.y], 'r.-')
 
-    for i in range(len(path)-1):
-        node_i = path[i]
-        node_ip1 = path[i+1]
-        plt.plot([node_i.x, node_ip1.x], [node_i.y, node_ip1.y], 'r.-')
-
-    plt.axis("equal")
-    plt.show()
+        plt.axis("equal")
+        plt.show()
+    else:
+        print(" [-] Failed to find path.")
--- 04_PathPlanning/ex08_rrt_star_nx.py
+++ 04_PathPlanning/ex08_rrt_star_nx.py
@@ -5,68 +5,84 @@ from map_5 import map
 
 class RRTStar(object):
     def __init__(self, start, goal, config):
-        self.G = nx.DiGraph() # Directed Graph
-        self.G.add_nodes_from([(-1, {'cost': 0, 'x': start[0], 'y': start[1]})]) # Add Start node [(Node ID, {'Cost', 'x', 'y'})]
+        self.G = nx.DiGraph()
+        self.G.add_nodes_from([(-1, {'cost': 0, 'x': start[0], 'y': start[1]})])
         self.start = start
         self.goal = goal
         self.config = config
 
-    # Random point generation
     def sample_free(self, obstacles, space):
         min_x, max_x, min_y, max_y = space
-        # Code
+        if np.random.rand() < self.config["goal_sample_rate"]:
+            return np.array([self.goal[0], self.goal[1]])
+        rand_x = np.random.uniform(min_x, max_x)
+        rand_y = np.random.uniform(min_y, max_y)
         return np.array([rand_x, rand_y])
 
-    # Search nearest node
     def get_nearest(self, rand_node):
-        # Code
-        return nearest_node_id
+        min_dist = float('inf')
+        nearest_id = None
+        for node_id in self.G.nodes:
+            node = self.get_node(node_id)
+            dist = np.linalg.norm(rand_node - node)
+            if dist < min_dist:
+                min_dist = dist
+                nearest_id = node_id
+        return nearest_id
 
-    # Node connection
     def steer(self, node_from, node_to, u=None):
-        # Code
-        return new_x, new_y
+        if u is None:
+            u = self.config["eta"]
+        direction = node_to - node_from
+        distance = np.linalg.norm(direction)
+        if distance <= u:
+            return node_to
+        direction = direction / distance
+        return node_from + u * direction
 
-    # Returns node(2d-array with position info.) corresponding to the node id
     def get_node(self, node_id):
         node = np.array([self.G.nodes[node_id]['x'], self.G.nodes[node_id]['y']])
         return node
 
-    # Collision Check
     def is_collision_free(self, node_from, node_to, obstacles, step=0.2):
-        # Code
-        # Collision check step size : u
-        # Collision check : path(connection) between 2 nodess
-        return False
+        direction = node_to - node_from
+        distance = np.linalg.norm(direction)
+        steps = int(distance / step)
+        for i in range(steps + 1):
+            x = node_from[0] + direction[0] * i / steps
+            y = node_from[1] + direction[1] * i / steps
+            for obs in obstacles:
+                if obs.is_inside(x, y):
+                    return False
+        return True
 
-    # Find adjacent nodes
     def get_near_node_ids(self, new_node, draw):
-        # Code
-        # Recommendation : Searching distance proportional to log(# of nodes in tree)/(# of nodes in tree)
-        return near_node_ids
+        n = len(self.G.nodes)
+        r = self.config["gamma_rrt_star"] * np.sqrt(np.log(n) / n)
+        near_ids = []
+        for node_id in self.G.nodes:
+            node = self.get_node(node_id)
+            if np.linalg.norm(new_node - node) <= r:
+                near_ids.append(node_id)
+        return near_ids
 
-    # Add node to tree
     def add_node(self, node_id, x, y):
         self.G.add_node(node_id, x=x, y=y)
 
-    # Get cost of designated node
     def get_node_cost(self, node_id):
         return self.G.nodes[node_id]['cost']
 
-    # Calculate the distance between 2 nodes
     def get_distance(self, node_from_id, node_to_id):
-        # Code
-        return dist
+        node_from = self.get_node(node_from_id)
+        node_to = self.get_node(node_to_id)
+        return np.linalg.norm(node_to - node_from)
 
-    # Add edge(Connection) between 2 nodes
     def add_edge(self, node_from_id, node_to_id):
         self.G.add_edge(node_from_id, node_to_id)
 
-    # Set cost to the node
     def set_node_cost(self, node_id, cost):
         self.G.nodes[node_id]['cost'] = cost
 
-    # Get parent node of designated node : predecessors
     def get_parent(self, node_id):
         parents = list(self.G.predecessors(node_id))
         if len(parents) > 0:
@@ -74,25 +90,19 @@ class RRTStar(object):
         else:
             return None
 
-    # Delete connections between 2 nodes
     def remove_edge(self, node_from_id, node_to_id):
-        self.G.remove_edge(node_from_id, node_to_id)
+        if self.G.has_edge(node_from_id, node_to_id):
+            self.G.remove_edge(node_from_id, node_to_id)
 
-    # Check goal
     def check_goal_by_id(self, node_id):
         node = self.G.nodes[node_id]
         dx = node['x'] - self.goal[0]
         dy = node['y'] - self.goal[1]
         dist = np.hypot(dx, dy)
-        # Regard as destination, if distance between the node and the goal is smaller than threshold.
-        if dist < self.config["goal_range"]:
-            return True
-        else:
-            return False
+        return dist < self.config["goal_range"]
 
 
 if __name__ == '__main__':
-
     start, goal, space, obstacles = map()
     for obs in obstacles:
         obs.plot()
@@ -103,7 +113,7 @@ if __name__ == '__main__':
         "goal_sample_rate": 0.05,
         "min_u": 1.0,
         "max_u": 3.0,
-        "goal_range" : 1.0
+        "goal_range": 1.0
     }
 
     rrt_star = RRTStar(start, goal, config)
@@ -112,28 +122,22 @@ if __name__ == '__main__':
     goal_node_id = None
 
     for i in range(1000):
-    ## Create Random Node 
         rand_node = rrt_star.sample_free(obstacles, space)
-        # plt.plot(rand_node[0], rand_node[1], '.')
-    ## Find Nearest Node
         nearest_node_id = rrt_star.get_nearest(rand_node)
         nearest_node = rrt_star.get_node(nearest_node_id)
-    ## Connect new node to the nearest node
         new_node = rrt_star.steer(nearest_node, rand_node)
-        # plt.plot(new_node[0], new_node[1], 's')
-    ## Check Collision of the new node
+
         if rrt_star.is_collision_free(nearest_node, new_node, obstacles):
-    ## Find adjacent nodes
             near_node_ids = rrt_star.get_near_node_ids(new_node, draw=True)
             rrt_star.add_node(i, new_node[0], new_node[1])
+
             if is_first_node:
                 rrt_star.add_edge(-1, i)
                 is_first_node = False
-            plt.plot(new_node[0], new_node[1], 's')
 
             min_node_id = nearest_node_id
-            min_cost = rrt_star.get_node_cost(nearest_node_id) + rrt_star.get_distance(i, nearest_node_id)
-    ## Find a node with minimum cost among adjacent nodes
+            min_cost = rrt_star.get_node_cost(nearest_node_id) + rrt_star.get_distance(nearest_node_id, i)
+
             for near_node_id in near_node_ids:
                 near_node = rrt_star.get_node(near_node_id)
                 if rrt_star.is_collision_free(near_node, new_node, obstacles):
@@ -145,7 +149,6 @@ if __name__ == '__main__':
             rrt_star.set_node_cost(i, min_cost)
             rrt_star.add_edge(min_node_id, i)
 
-    ## Rewire the tree with adjacent nodes
             for near_node_id in near_node_ids:
                 near_node = rrt_star.get_node(near_node_id)
                 if rrt_star.is_collision_free(new_node, near_node, obstacles):
@@ -155,33 +158,27 @@ if __name__ == '__main__':
                         if parent_node_id is not None:
                             rrt_star.remove_edge(parent_node_id, near_node_id)
                             rrt_star.add_edge(i, near_node_id)
-    ## Check Goal
+
             if rrt_star.check_goal_by_id(i):
                 goal_node_id = i
                 break
-    ## Plotting
+
     for e in rrt_star.G.edges:
         v_from = rrt_star.G.nodes[e[0]]
         v_to = rrt_star.G.nodes[e[1]]
-
         plt.plot([v_from['x'], v_to['x']], [v_from['y'], v_to['y']], 'b-')
-        plt.text(v_to['x'], v_to['y'], e[1])
+        #plt.text(v_to['x'], v_to['y'], str(e[1]), fontsize=6)
 
     if goal_node_id is not None:
         path = nx.shortest_path(rrt_star.G, source=-1, target=goal_node_id)
-        xs = []
-        ys = []
-
+        xs, ys = [], []
         for node_id in path:
             node = rrt_star.G.nodes[node_id]
             xs.append(node['x'])
             ys.append(node['y'])
         plt.plot(xs, ys, 'r-', lw=3)
 
-
-
     plt.plot(start[0], start[1], 'ro')
     plt.plot(goal[0], goal[1], 'bx')
-
     plt.axis("equal")
     plt.show()
